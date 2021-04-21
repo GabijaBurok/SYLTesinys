2106681af781f3573da9e3bf264ed4b01eab74f8
GabijaBurok <71337797+GabijaBurok@users.noreply.github.com>
2021-04-21 13:48:18 +0300

    ?

---
+++ SYL Mobile/SYL Mobile/Services/Secrets.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SYL.Mobile.Services
+{
+    internal class Secrets
+    {
+        public static string IP = "193.219.91.103:15819";
+    }
+}
